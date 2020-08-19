b30b37255d1e23b4a8d5a21ce74473abf3334e63
joao pedro <joaof082@gmail.com>
2020-08-19 18:55:04 -0300

    configurando o banco de dados em sqlite3

---
+++ server/READEM.md
@@ -0,0 +1,13 @@
+# Funcionalidades
+
+## conexões
+
+- Rotas para listar o total de conexões realizadas;
+- Roats pa criar uma nova conexão;
+
+
+##  Aulas
+
+- Rota para cria uma aula;
+- Rota para lsitar aulas;
+    - Filtrar por matéria, dia da semana, horário;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,10 @@ import express from 'express'
 
 const app = express();
 
-app.get('/users', (request, response) => {
+app.use(express.json());
 
-    const users = [
-        {name:'joao', age: 25 },
-        {name:"pedro", age: 16},
-    ]
-
-    return response.json(users)
-    console.log('acessou a rota');
+app.get('/', (request, response) => { 
+    return response.json({ message: "hello word"})
 });
 
 app.listen(3333);
